9ca62748778d26e74c7fa05b2dee07ff26cd2367
AvagaddaAnitha <165392785+AvagaddaAnitha@users.noreply.github.com>
2025-09-14 16:04:10 +0530

    College.py

--- College.py
+++ College.py
@@ -38,7 +38,8 @@ st.markdown("""
 # -----------------------------
 # 🔑 Configure Gemini API (securely with secrets)
 # -----------------------------
-genai.configure(api_key=st.secrets["AIzaSyB5MZ_sayQOjtiAePW_g6SPOLCS3Xpz3EY"])
+import os
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 gemini = genai.GenerativeModel("gemini-1.5-flash")
 
 # -----------------------------
